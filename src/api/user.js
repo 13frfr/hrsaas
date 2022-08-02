0d0ed7838e92abeab8908f68226bb2d618b7b5c5
13frfr <2458151214@qq.com>
2022-08-02 10:50:21 +0800

    完成登录流程

--- src/api/user.js
+++ src/api/user.js
@@ -1,4 +1,15 @@
 import request from '@/utils/request'
 
-export function login(data) {}
+/**
+ * 登录请求
+ * @param {Object} data mobile password
+ * @returns promise
+ */
+export function login(data) {
+    return request({
+        url:'/sys/login',
+        method: 'POST',
+        data
+    })
+}
 
