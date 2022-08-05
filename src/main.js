35ceb4057b0b1cb0aab9270845c10a42860d977b
13frfr <2458151214@qq.com>
2022-08-05 11:32:54 +0800

    将树形的操作内容单独抽提成组件

--- src/main.js
+++ src/main.js
@@ -14,8 +14,8 @@ import router from './router'
 
 import '@/icons' // icon
 import '@/permission' // permission control
-// 引入抽离的自定义指令
-import * as directives from '@/directives'
+  // 引入抽离的自定义指令
+  import * as directives from '@/directives'
 
 // mock假数据
 if (process.env.NODE_ENV === 'production') {
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,21 +1,45 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <el-card class="box-card">
+        <!-- 头部 -->
+        <TreeTools :isRoot="true" :treeNode="company"></TreeTools>
+        <!-- 树形 -->
+        <el-tree :data="treeData" :props="defaultProps">
+          <template v-slot="{ data }">
+            <TreeTools :treeNode="data"></TreeTools>
+          </template>
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import TreeTools from "./components/tree-tools.vue";
 export default {
   data() {
-    return {}
+    return {
+      treeData: [
+        { name: "总裁办", children: [{ name: "董事会" }] },
+        { name: "行政部" },
+        { name: "人事部" },
+      ],
+      defaultProps: {
+        label: "name", // 将data想显示的名字显示到树形
+      },
+      company: { name: "传智教育", manager: "负责人" },
+    };
+  },
+
+  components: {
+    TreeTools,
   },
 
   created() {},
 
-  methods: {}
-}
+  methods: {},
+};
 </script>
 
 <style scoped lang="less"></style>
